afda1021784a3ea174a957b3010efd1ad0011481
Jennie Ji <jennie.ji@hotmail.com>
2013-09-08 14:16:42 +0800

    Fix bugs, add demo

--- jquery.niceSelect.js
+++ jquery.niceSelect.js
@@ -16,25 +16,33 @@
 				selectOpen: PREFIX + 'open',
 				dropdown: 	PREFIX + 'dropdown',
 				disabled: 	PREFIX + 'disabled'
-			},SELECTOR;
+			},
+            SELECTOR = {};
 
         var wrapper = '<div class="' + CLASS.wrapper + '" />';
 
         // functions
+        var isInited = function( $select ){
+            return $select.parent( SELECTOR.wrapper ).length>0;
+        }
 		var destroy = function( ){
-			$selects.siblings().remove();
-			$selects.unwrap();
+            $selects.each(function(){
+                var $select = $(this);
+                if( !isInited( $select ) ) { return; }
+    			$selects.siblings().remove();
+    			$selects.unwrap().show();
+            });
 			return this;
 		};
         var hideSelect = function(){
             $(SELECTOR.dropdown).hide();
         };
 		var init = function( ){
-            if( !$selets || !$selets.size() ) { return; }
+            if( $selects.length<1 ) { return; }
 
-            $selets.each(function(){
+            $selects.each(function(){
                 var $select = $(this);
-            	if( $select.parent( SELECTOR.wrapper ).length>1 ) { return; }
+            	if( isInited( $select ) ) { return; }
 
                 var $options = $select.find('OPTION');
                 var $selected = $select.find(':selected').length<1 ? $select.first() : $select.find(':selected');
@@ -45,9 +53,9 @@
                 var isDisabled = $select.is(':disabled');
 
                 // build HTML
-                var newSelect = '<div class="' + CLASS.select + ( isDisabled ? ' niceSelect-disabled' : '' ) + '" style="width:' + selectWidth +'px; height:' + selectHeight +  ' position:relative;"></div>',
-	                newSelectOpen = '<span class="' + CLASS.selectOpen + '" style="float:right; height:' + selectHeight +  ';"></span>',
-	                newSelectText = '<span class="' + CLASS.selectText + '" style="height:' + selectHeight +  '; overflow:hidden;">' + $selected.text() + '</span>',
+                var newSelect = '<div class="' + CLASS.select + ( isDisabled ? ' niceSelect-disabled' : '' ) + '" style="width:' + selectWidth +'px; height:' + selectHeight +  'px; position:relative;"></div>',
+	                newSelectOpen = '<div class="' + CLASS.selectOpen + '" style="float:right; height:' + selectHeight +  'px;"></div>',
+	                newSelectText = '<div class="' + CLASS.selectText + '" style="height:' + selectHeight +  'px; overflow:hidden;">' + $selected.text() + '</div>',
 	                newDropdown = function(){
 	                	return '<ul class="' + CLASS.dropdown + '" style="display:none; position:absolute; z-index:' + opts.zindex + ';"></ul>';
 	                };
@@ -59,7 +67,7 @@
                 $newSelectText = $( newSelectText ).appendTo( $newSelect );
 
                 // set style
-                $newSelectText.width( selectWidth - $newSelectOpen.outerWidth(true) - $newSelectText.outerWidth() );
+                // $newSelectText.width( selectWidth - $newSelectOpen.outerWidth(true) - $newSelect.outerWidth() );
 
 
                 if(!isDisabled){
@@ -76,14 +84,14 @@
                     $newSelectList.width( selectWidth );
 
                     // open the dropdown list
-                    $newSelect.children().click( function(){
+                    $newSelectText.add( $newSelectOpen ).click( function(){
                         hideSelect();
                         $newSelectList.slideDown('fast');
                     });
                     // change the select's value
                     $newSelectList.delegate('A','click',function(e){
                         e.preventDefault();
-                        $newSelectText.text( $(this).text() );
+                        $newSelectText.html( $(this).text() );
                     	$select.val( $(this).parent().data('value') );
                        	$select.change();
                         hideSelect();
@@ -92,7 +100,7 @@
             });
 			// select blur effect
 			$(document).mousedown(function(e) {
-                if ($(e.target).parents( CLASS.wrapper ).length === 0) {
+                if ($(e.target).parents( SELECTOR.wrapper ).length === 0) {
                 	hideSelect();
                 }
             });
